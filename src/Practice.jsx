526a9e1822f8adff34cf9716beb864c8f16dba95
Vrinda Sharma <vrinda26sharma@gmail.com>
2024-03-09 13:47:05 +0530

    initial commit

--- src/Practice.jsx
+++ src/Practice.jsx
@@ -1,3 +1,20 @@
+import React , {createContext} from "react";
+
+const FirstName = createContext();
+
+const Practice = () => {
+    return (<>
+          <FirstName.Provider value={"Vrinda"}>
+            
+          </FirstName.Provider>
+    </>);
+}
+
+export default Practice;
+
+
+/*
+For applying dynamic time to website
 import React, { useState } from "react";
 
 const Practice = () => {
@@ -14,15 +31,4 @@ const Practice = () => {
 
 export default Practice;
 
-
-/*
-For applying dynamic time to website
-const [time,setTime] = useState(new Date().toLocaleTimeString());
-    
-    setInterval(() => {
-        setTime(new Date().toLocaleTimeString());
-    },1000);
-    return (<>
-          <h1>{time}</h1>
-    </>);
 */
